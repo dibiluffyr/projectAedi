0a6775460d3702e2d2c4acd516c1b29d69a09f93
radion1x <leeduken96@gmail.com>
2024-10-19 11:34:37 +0800

    1- auth controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,107 @@
-import express from  'express';
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
-    res.json({
-        data: "You hit the signup endpoint",
-    });
+    try {
+        const { username, password, email } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email formal" });
+        }
+
+        const passwordFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>\/?~`-])[A-Za-z\d!@#$%^&*()_+[\]{};':"\\|,.<>\/?~`-]{8,}$/;
+        if (!passwordFormat.test(password)) {
+            return res.status(400).json({ error: "Password must be at least 8 characters with 1 uppercase, 1 lowercase, 1 number, and 1 special character" });
+
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ error: "Username already in use" });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already in use" })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                totalNice: newUser.totalNice,
+                followers: newUser.followers,
+                following: newUser.following,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+        
+    }
 }
+
 export const login = async (req, res) => {
-    res.json({
-        data: "You hit the login endpoint",
-    });
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(201).json({
+            _id: user._id,
+            email: user.email,
+            totalNice: user.totalNice,
+            followers: newUser.followers,
+            following: newUser.following,
+        });
+
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 export const logout = async (req, res) => {
-    res.json({
-        data: "You hit the logout endpoint",
-    });
+    try {
+        res.cookie("jwt", "", {maxAge:0});
+        res.status(200).json({ messsage: "Logout successfully" });
+    } catch (error) {
+        console.log("Error in logout controller",  error.message);
+        res.status(500).json({ error: "Internal server error" });
+
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in getMe controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
---
+++ backend/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                // Regular expression to check for at least one lowercase letter, one uppercase letter, one number, and one special character
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>\/?~`-])[A-Za-z\d!@#$%^&*()_+[\]{};':"\\|,.<>\/?~`-]{8,}$/.test(v);
+            },
+            message: props => `Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character!`
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    totalNice: {
+        type: Number,
+        default: 0
+    }
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User
