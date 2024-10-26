5b8a1112c1fbe4a05aee69e5659603ba73c0000e
radion1x <leeduken96@gmail.com>
2024-10-26 13:21:12 +0800

    3- fetch posts for the home page

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -7,7 +7,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Post = ({ post }) => {
-	const [comment, setComment] = useState("");
+	const [adaptEdit, setAdaptEdit] = useState("");
+	const [adaptNext, setAdaptNext] = useState("");
 	const postOwner = post.user;
 	const isLiked = false;
 
@@ -15,15 +16,18 @@ const Post = ({ post }) => {
 
 	const formattedDate = "1h";
 
-	const isCommenting = false;
+	const isAdaptEditing = false;
+
+	const [showAdaptEdits, setShowAdaptEdits] = useState(false);
+	const [showAdaptNexts, setShowAdaptNexts] = useState(false);
 
 	const handleDeletePost = () => {};
 
-	const handlePostComment = (e) => {
+	const handlePostAdaptEdit = (e) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleAdaptEditPost = () => {};
 
 	return (
 		<>
@@ -55,96 +59,63 @@ const Post = ({ post }) => {
 							<img
 								src={post.img}
 								className='h-80 object-contain rounded-lg border border-gray-700'
-								alt=''
 							/>
 						)}
-					</div>
-					<div className='flex justify-between mt-3'>
-						<div className='flex gap-4 items-center w-2/3 justify-between'>
-							<div
-								className='flex gap-1 items-center cursor-pointer group'
-								onClick={() => document.getElementById("comments_modal" + post._id).showModal()}
-							>
-								<FaRegComment className='w-4 h-4  text-slate-500 group-hover:text-sky-400' />
-								<span className='text-sm text-slate-500 group-hover:text-sky-400'>
-									{post.comments.length}
-								</span>
-							</div>
-							{/* We're using Modal Component from DaisyUI */}
-							<dialog id={`comments_modal${post._id}`} className='modal border-none outline-none'>
-								<div className='modal-box rounded border border-gray-600'>
-									<h3 className='font-bold text-lg mb-4'>COMMENTS</h3>
-									<div className='flex flex-col gap-3 max-h-60 overflow-auto'>
-										{post.comments.length === 0 && (
-											<p className='text-sm text-slate-500'>
-												No comments yet 🤔 Be the first one 😉
-											</p>
-										)}
-										{post.comments.map((comment) => (
-											<div key={comment._id} className='flex gap-2 items-start'>
-												<div className='avatar'>
-													<div className='w-8 rounded-full'>
-														<img
-															src={comment.user.profileImg || "/avatar-placeholder.png"}
-														/>
-													</div>
-												</div>
-												<div className='flex flex-col'>
-													<div className='flex items-center gap-1'>
-														<span className='font-bold'>{comment.user.fullName}</span>
-														<span className='text-gray-700 text-sm'>
-															@{comment.user.username}
-														</span>
-													</div>
-													<div className='text-sm'>{comment.text}</div>
-												</div>
-											</div>
-										))}
+						{showAdaptEdits && post.adaptEdits.length > 0 && (
+							<div className='flex flex-col gap-2'>
+								{post.adaptEdits.map((adaptEdit) => (
+									<div key={adaptEdit._id}>
+										<p className='text-sm text-gray-500'>{adaptEdit.text}</p>
 									</div>
-									<form
-										className='flex gap-2 items-center mt-4 border-t border-gray-600 pt-2'
-										onSubmit={handlePostComment}
-									>
-										<textarea
-											className='textarea w-full p-1 rounded text-md resize-none border focus:outline-none  border-gray-800'
-											placeholder='Add a comment...'
-											value={comment}
-											onChange={(e) => setComment(e.target.value)}
-										/>
-										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
-											) : (
-												"Post"
-											)}
-										</button>
-									</form>
-								</div>
-								<form method='dialog' className='modal-backdrop'>
-									<button className='outline-none'>close</button>
-								</form>
-							</dialog>
-							<div className='flex gap-1 items-center group cursor-pointer'>
-								<BiRepost className='w-6 h-6  text-slate-500 group-hover:text-green-500' />
-								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
+								))}
 							</div>
-							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
-									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
-								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
-
-								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
-									}`}
+						)}
+						{showAdaptNexts && post.adaptNexts.length > 0 && (
+							<div className='flex flex-col gap-2'>
+								{post.adaptNexts.map((adaptNext) => (
+									<div key={adaptNext._id}>
+										<p className='text-sm text-gray-500'>{adaptNext.text}</p>
+									</div>
+								))}
+							</div>
+						)}
+						<div className='flex justify-between items-center'>
+							<div className='flex gap-2 items-center'>
+								<Link to={`/post/${post._id}`} className='text-gray-700'>
+									<FaRegComment className='text-lg' />
+								</Link>
+								<Link to={`/post/${post._id}`} className='text-gray-700'>
+									<BiRepost className='text-lg' />
+								</Link>
+								<Link to={`/post/${post._id}`} className='text-gray-700'>
+									<FaRegHeart className='text-lg' />
+								</Link>
+								<Link to={`/post/${post._id}`} className='text-gray-700'>
+									<FaRegBookmark className='text-lg' />
+								</Link>
+							</div>
+							<div className='flex gap-2 items-center'>
+								<Link
+									to={`/post/${post._id}`}
+									className='text-gray-700'
+									onClick={() => setShowAdaptEdits(!showAdaptEdits)}
 								>
-									{post.likes.length}
-								</span>
+								</Link>
+								<Link
+									to={`/post/${post._id}`}
+									className='text-gray-700'
+									onClick={() => setShowAdaptEdits(!showAdaptEdits)}
+								>
+									{post.adaptEdits.length} Adapt Edits
+								</Link>
+								<Link
+									to={`/post/${post._id}`}
+									className='text-gray-700'
+									onClick={() => setShowAdaptNexts(!showAdaptNexts)}
+								>
+									{post.adaptNexts.length} Adapt Nexts
+								</Link>
 							</div>
-						</div>
-						<div className='flex w-1/3 justify-end gap-2 items-center'>
-							<FaRegBookmark className='w-4 h-4 text-slate-500 cursor-pointer' />
 						</div>
 					</div>
 				</div>
@@ -152,4 +123,5 @@ const Post = ({ post }) => {
 		</>
 	);
 };
+
 export default Post;
