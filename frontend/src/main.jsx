7e2b791331e0c4282da7b6911bcd5c4e031d8f51
radion1x <leeduken96@gmail.com>
2024-10-26 10:50:57 +0800

    3- Auth handled and routes protected in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'  
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <QueryClientProvider client = {queryClient}>
+     <App />
+    </QueryClientProvider>
     </BrowserRouter>
-
   </React.StrictMode>,
-)
+);
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -6,6 +6,8 @@ import AediSvg from "../../../components/svgs/Aedi";
 import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 
 const SignUpPage = () => {
@@ -15,16 +17,42 @@ const SignUpPage = () => {
 		password: "",
 	});
 
+	const { mutate, isError, isPending, error} = useMutation({
+		mutationFn: async({email, username, password}) => {
+			try {
+				const res = await fetch("/api/auth/signup", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({email, username, password}),
+				});
+
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				
+				console.log(data);
+				return data;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		},
+		onSuccess: () => {
+			toast.success("Account created successfully");
+		},
+	})
+
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		mutate(formData);
 	};
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const isError = false;
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
@@ -70,8 +98,10 @@ const SignUpPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>Sign up</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
+					<button className='btn rounded-full btn-primary text-white'>
+						{isPending ? "Signing up..." : "Sign up"}
+					</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
 					<p className='text-white text-lg'>Already have an account?</p>
